0048c8b8ee7c3feeecda7a633dbe98d48ab13234
BCemreD <72667928+BCemreD@users.noreply.github.com>
2021-04-09 00:25:03 +0300

    User, Customer, Rental entities, methods

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -12,5 +12,18 @@ namespace Business.Constants
         public static string CarUpdated = "Araç Güncellendi.";
         public static string CarDeleted = "Araç Silindi";
         public static string MaintenanceTime = "Sistem bakımda";
+
+        public static string UserAdded = "Kullanıcı eklendi.";
+        public static string UserUpdated = "Kullanıcı güncellendi.";
+        public static string UserDeleted = "Kullanıcı silindi.";
+
+        public static string CustomerAdded = "Müşteri eklendi.";
+        public static string CustomerUpdated = "Müşteri güncellendi.";
+        public static string CustomerDeleted = "Müşteri silindi.";
+
+        public static string RentalAdded = "Kiralama işlemi eklendi.";
+        public static string RentalUpdated = "Kiralama işlemi güncellendi.";
+        public static string RentalDeleted = "Kiralama işlemi silindi.";
+        public static string RentalUndeliveredCar = "Araç henüz teslim edilmedi.";
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Concrete;
+using Core.Utilities.Results;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
@@ -8,21 +9,49 @@ namespace ConsoleUI
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new EfProductDal());      
 
-            Product product = new Product
-            { Name="Renault", Id = 5, BrandId = 3, ColorId = 1, ModelYear = 2020, DailyPrice = 340000, Description = "Mavi Renault Zoe" };
-            productManager.Add(product);
+            //Product product = new Product
+            //{ Name = "Renault", Id = 5, BrandId = 3, ColorId = 1, ModelYear = 2020, DailyPrice = 340000, Description = "Mavi Renault Zoe" };
+           
             //productManager.GetAll().ForEach(p => Console.WriteLine(p.Name));
             //productManager.GetProductDetails().ForEach(p => Console.WriteLine("{0}{1}{2}{3}", p.Name, p.BrandName, p.ColorName, p.DailyPrice));
-            productManager.GetAll().Data.ForEach(p => Console.WriteLine(p.Name));
-            productManager.GetProductDetails().Data.ForEach(p => Console.WriteLine("{0}{1}{2}{3}", p.Name, p.BrandName, p.ColorName, p.DailyPrice));
-
-
-
+                   
+        }
+        private static void rentalTest()
+        {
+            RentalManager rentalManager = new RentalManager(new EfRentalDal());
+            Rental rental = new Rental
+            {
+                RentalId = 4,
+                CustomerId = 3,
+                RentDate = DateTime.Now,
+                ReturnDate = null
+            };
+            IResult result = rentalManager.Add(rental);
+            if (!result.Success) Console.WriteLine(result.Message);
+            rentalManager.GetAll().Data.ForEach(r => Console.WriteLine(r.RentalId + " " + r.RentDate));
+        }
+        private static void productTest()
+            {
+                ProductManager productManager = new ProductManager(new EfProductDal());
+            productManager.GetProductDetails().Data.ForEach(p => 
+            Console.WriteLine("{0}{1}{2}{3}", p.Name, p.BrandName, p.ColorName, p.DailyPrice));
 
         }
+        private static void userTest()
+            {
+                UserManager userManager = new UserManager(new EfUserDal()); 
+            }
+        private static void customerUser()
+        {
+            CustomerManager customerManager = new CustomerManager(new EfCustomerDal());
+            Customer customer = new Customer { UserId = 2, CompanyName = "Renault" };
+            customerManager.Add(customer);
+            customerManager.GetAll().Data.ForEach(c => Console.WriteLine(c.CompanyName));
+        }
+               
+          
     }
 }
